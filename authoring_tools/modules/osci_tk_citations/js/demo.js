b60869a2e83264ca437597b959fe20b5b27aee6f
Gray Bowman <gbowman@imamuseum.org>
2012-01-31 11:05:57 -0500

    Created Citation module and demo functionality. Will return once body copy fields are stabilized

---
+++ authoring_tools/modules/osci_tk_citations/js/demo.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function() {
+	var $ = jQuery;
+	
+	// override the Retrieve submit
+	var ret_submit = $('#edit-submit');
+	ret_submit.click(function(event) {
+		event.preventDefault();
+		var data = {
+			'book_id'		: $('#edit-book-id').val(),
+			'section_id'	: $('#edit-section-id').val(),
+			'paragraph_id'	: $('#edit-paragraph-id').val()
+		};
+		var target = $('#ret-result');
+		retrieve_note(data, target);
+	});
+	
+	
+});
+
+function retrieve_note(data, target) {
+	var $ = jQuery;
+	var endpoint = Drupal.settings.basePath + 'citations/';
+	
+	$.get(endpoint, data, function(data) {
+		target.html(data);
+	})
+}
