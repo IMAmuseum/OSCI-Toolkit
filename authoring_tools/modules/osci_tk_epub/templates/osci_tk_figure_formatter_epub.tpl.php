1d69490cb2c278213748e5ae3301118f44c1cc56
Ed Bachta <ebachta@imamuseum.org>
2012-02-17 15:33:52 -0500

    Adding media type for figures based on content type

--- authoring_tools/modules/osci_tk_epub/templates/osci_tk_figure_formatter_epub.tpl.php
+++ authoring_tools/modules/osci_tk_epub/templates/osci_tk_figure_formatter_epub.tpl.php
@@ -1,5 +1,5 @@
 <object id="<?php print $item['fig_id']; ?>" 
-	type="application/osci-tk-conservation-figure" 
+	type="<?= $media_type ?>" 
 	data="<?php print $item['content_url']; ?>">
 	<div class="figure-reference">
 		<!-- <img src="http://www.cgexplorer.com/_sys/images/female-figure-reference.jpg"/> -->
