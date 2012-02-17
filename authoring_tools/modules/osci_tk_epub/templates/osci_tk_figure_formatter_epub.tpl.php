a1000686dc6e1c503265d49a8df11193eae80e6f
Kyle Jaebker <kjaebker@gmail.com>
2012-02-17 14:41:33 -0500

    initial pass at adding figures to epub content output
    This needs extended a bit, but is a good first pass.

---
+++ authoring_tools/modules/osci_tk_epub/templates/osci_tk_figure_formatter_epub.tpl.php
@@ -0,0 +1,7 @@
+<object id="<?php print $item['fig_id']; ?>" 
+	type="application/osci-tk-conservation-figure" 
+	data="<?php print $item['content_url']; ?>">
+	<div class="figure-reference">
+		<!-- <img src="http://www.cgexplorer.com/_sys/images/female-figure-reference.jpg"/> -->
+	</div>
+</object>
