0d6a0404b9033e6f67c4f5e53261f493358e1446
Ed Bachta <ebachta@imamuseum.org>
2012-02-23 14:53:56 -0500

    Implemented a new scheme to render figures

--- authoring_tools/modules/osci_tk_figure/templates/osci_tk_figure_formatter.tpl.php
+++ authoring_tools/modules/osci_tk_figure/templates/osci_tk_figure_formatter.tpl.php
@@ -1,4 +1,4 @@
 <figure id="<?php print $item['fig_id']; ?>" data-position="<?php print $item['position']; ?>" data-columns="<?php print $item['columns']; ?>" data-options="<?php print htmlspecialchars($item['options']); ?>">
-	<?= $figure_content ?>
+	<div class='figure_content'><?= $figure_content ?></div>
 	<figcaption><?php print $item['number_template'] . ' ' . $item['caption']; ?></figcaption>
 </figure>
