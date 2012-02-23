d24d9f45aba29ea52521af731322d7009232c388
Gray Bowman <gbowman@imamuseum.org>
2012-02-23 16:05:15 -0500

    footnote ckeditor button - in progress

--- authoring_tools/modules/osci_tk_filters/js/dialogs/footnote.js
+++ authoring_tools/modules/osci_tk_filters/js/dialogs/footnote.js
@@ -1,62 +1,139 @@
 ( function($) {
-    function footnoteDialog(editor) {
-        var buttons = [ CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton ];
-    
-        var elements = [{
-            type:           'select',
-            id:             'existingFootnote',
-            items:          [['', '']],
-            label:          'Select a footnote',
-            labelLayout:    'horizontal',
-            onShow:         existingFootnoteShow,
-            editorId:       editor.name
-        }];
+	var dialogOptions = {
+		'zIndex': 10011,
+		'modal': true,
+		'width': 700
+	};
+	
+	function footnoteDialog(editor) {
+		var buttons = [ CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton ];
+	
+		var elements = [{
+			type:           'select',
+			id:             'existingFootnote',
+			items:          [['', '']],
+			label:          'Select a footnote',
+			labelLayout:    'horizontal',
+			onShow:         existingFootnoteShow,
+			editorId:       editor.name,
+			className:      'existingFootnote',
+			onChange:		function(api) {
+				if (api.data.value == "new") {
+					$('.newFootnote').show();
+				}
+				else {
+					$('.newFootnote').hide();
+				}
+			}
+		},
+		{
+			type : 'textarea',
+			id : 'newFootnote',
+			label: 'Footnote:',
+			className: 'newFootnote',
+			style: 'display: none;',
+			rows : 4,
+			cols : 30
+		}
+		];
 
-        var contents = [{
-            id:     'footnote',
-            label:  'Add a footnote reference',
-            elements:   elements
-        }] 
-        
-        return {
-            title: 'Add a footnote reference',
-            minWidth:   400,
-            minHeight:  100,
-            buttons:    buttons,
-            resizable:  'none',
-            contents:   contents,
-            onOk:       footnoteOk
-        }
-    }
+		var contents = [{
+			id:         'footnote',
+			label:      'Add a footnote reference',
+			elements:   elements
+		}] 
+		
+		return {
+			title:      'Add a footnote reference',
+			minWidth:   400,
+			minHeight:  100,
+			buttons:    buttons,
+			resizable:  'none',
+			contents:   contents,
+			onOk:       footnoteOk
+		}
+	}
 
-    function existingFootnoteShow(data) {
-        var items = '';
-        var id = '#' + $(this.getInputElement().$).attr('id');
-        var footnotes = $('.footnote_identifier');
-
-        footnotes.each(function(idx) {
+	function existingFootnoteShow(data) {
+		var items = '';
+		var id = '#' + $(this.getInputElement().$).attr('id');
+		// get all the footnote identifiers and create a select options for them
+		var footnotes = $('.footnote_identifier');
+		footnotes.each(function(idx) {
 			var $this = $(this);
-            items = items + '<option value="' + $this.data("fnId") + '">' + (idx) + ' (' + $this.data("fnId") + ')</option>';
-        });
+			items = items + '<option value="' + $this.data("fnId") + '">' + (idx) + ' (' + $this.data("fnId") + ')</option>';
+		});
+		// append a new option
+		items = items + '<option value="new">New Footnote</option>';
+		// replace the options
+		$(id).html('');
+		$(id).append(items);
+		// if there weren't any existing footnotes found, unhide the new footnote textarea by default
+		if (footnotes.length == 0) {
+			$('.newFootnote').show();
+		}
+	}
 
-        $(id).html('');
-        $(id).append(items);
-    }
-
-    function footnoteOk(data) {
-        var editor = this.getParentEditor();
-        var dialog = CKEDITOR.dialog.getCurrent();
-        var existingFootnote = dialog.getValueOf('footnote', 'existingFootnote');
-    
-        if (existingFootnote != '') {
-            var replace = existingFootnote;
-        }
-
-        editor.insertText('[footnote:' + replace + ']');
-    }
-
-    CKEDITOR.dialog.add('footnote', function(editor) {
-        return footnoteDialog(editor);
-    });
+	function footnoteOk(data) {
+		var editor = this.getParentEditor();
+		var dialog = CKEDITOR.dialog.getCurrent();
+		var existingFootnote = dialog.getValueOf('footnote', 'existingFootnote');
+		var replace = existingFootnote;
+		
+		// if the user selected new, create a new footnote, populate it, and change 'replace'
+		// to the proper footnote reference
+		if (existingFootnote == "new") {
+			var newFootnote = dialog.getValueOf('footnote', 'newFootnote');
+			console.log(newFootnote, 'newFootnote')
+			
+		}
+		editor.insertText('[footnote:' + replace + ']');
+	}
+	
+	/*
+	function addNewFootnote(data) {
+		// count the number of footnote tabs
+		var tabCountStart = $('#fieldset-tabs-field_footnote').find('ul.ui-tabs-nav').find('li').length;
+		console.log(tabCountStart, 'tabCountStart');
+		// create new footnote tab
+		$('input[id^="edit-field-footnote-und-add-more"]').trigger('mousedown');
+		// wait in a loop until the tab count goes up
+		var interval = setInterval(function() {
+			var tabCount = $('#fieldset-tabs-field_footnote').find('ul.ui-tabs-nav').find('li').length;
+			console.log(tabCount, 'tabCount');
+			if (tabCount > tabCountStart) {
+				clearInterval(interval);
+				// new tab detected, get form contents
+				console.log('new tab detected');
+				tabContents = $('#fieldset-tab-edit-field-footnote-und-' + (tabCount - 1)).remove();
+				console.log(tabContents, 'tabContents');
+				// place into modal
+				$(tabContents).dialog({
+					'title':   'Add a Footnote',
+					'zIndex':  10011,
+					'modal':   true,
+					'width':   700,
+					'buttons': {
+						'Ok': function() {
+							console.log(this);
+						},
+						'Cancel': function() {
+							console.log(this);
+						}
+					},
+				});
+				
+			}
+		}, 1000)
+		
+		
+		// $('<div></div>').dialog(dialogOptions);
+		
+	}
+	*/
+	
+	CKEDITOR.dialog.add('footnote', function(editor) {
+		return footnoteDialog(editor);
+	});
 
 })(jQuery);
