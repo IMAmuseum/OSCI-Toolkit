025c970c3b3cbed30dbcd3d7f654a7d47c3bbac1
Gray Bowman <gbowman@imamuseum.org>
2012-02-24 14:33:05 -0500

    working on ckeditor custom commands.  fixed some permissions

--- authoring_tools/modules/osci_tk_filters/js/dialogs/figure.js
+++ authoring_tools/modules/osci_tk_filters/js/dialogs/figure.js
@@ -1,66 +1,81 @@
 ( function($) {
-    function figureDialog(editor) {
-        var buttons = [ CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton ];
-        var figures = $('.figure_identifier');
-        var items = [['', '']];
-    
-        figures.each(function(idx) {
+	function figureDialog(editor) {
+		var buttons = [ CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton ];
+		var figures = $('.figure_identifier');
+		var items = [['', '']];
+		
+		figures.each(function(idx) {
 			var $this = $(this);
-            items.push([(idx) + ' (' + $this.data("figId") + ')', $this.data("figId")]);
-        });
+			items.push([(idx) + ' (' + $this.data("figId") + ')', $this.data("figId")]);
+		});
+		
+		var elements = [{
+			type:           'text',
+			id:             'figureId',
+			label:          'Enter a figure ID',
+			labelLayout:    'horizontal'
+		},
+		{
+			type:           'html',
+			html:           '<b>OR</b>'
+		},
+		{
+			type:           'select',
+			id:             'existingFigure',
+			items:          items,
+			label:          'Select an existing figure',
+			labelLayout:    'horizontal'
+		},
+		{
+			type:           'html',
+			html:           '<b>OR</b>'
+		},
+		{
+			type:           'button',
+			id:             'addNewButton',
+			label:          'Add New Figure',
+			onClick:        addNewFigure
+		}];
+		
+		var contents = [{
+			id:             'figure',
+			label:          'Add figure',
+			elements:       elements
+		}] 
+		
+		return {
+			title:          'Add a figure',
+			minWidth:       400,
+			minHeight:      100,
+			buttons:        buttons,
+			resizable:      'none',
+			contents:       contents,
+			onOk:           figureOk
+		}
+	}
 
-        var elements = [{
-            type:           'text',
-            id:             'figureId',
-            label:          'Enter a figure ID',
-            labelLayout:    'horizontal'
-        },
-        {
-            type:           'html',
-            html:           '<b>OR</b>'
-        },
-        {
-            type:           'select',
-            id:             'existingFigure',
-            items:          items,
-            label:          'Select an existing figure',
-            labelLayout:    'horizontal'
-        }];
-
-        var contents = [{
-            id:     'figure',
-            label:  'Add figure',
-            elements:   elements
-        }] 
-        
-        return {
-            title: 'Add a figure',
-            minWidth:   400,
-            minHeight:  100,
-            buttons:    buttons,
-            resizable:  'none',
-            contents:   contents,
-            onOk:       figureOk
-        }
-    }
-
-    function figureOk(data) {
-        var editor = this.getParentEditor(),
+	function figureOk(data) {
+		var editor = this.getParentEditor(),
 			dialog = CKEDITOR.dialog.getCurrent(),
 			figureId = dialog.getValueOf('figure', 'figureId'),
 			existingFigure = dialog.getValueOf('figure', 'existingFigure'),
 			replace;
-    
-        if (existingFigure != '') {
-            replace = existingFigure;
-        } else if (figureId != '') {
-            replace = figureId;
-        }
-
-        editor.insertText('[figure:' + replace + ']');
-    }
-
-    CKEDITOR.dialog.add('figure', function(editor) {
-        return figureDialog(editor);
-    });
+	
+		if (existingFigure != '') {
+			replace = existingFigure;
+		} else if (figureId != '') {
+			replace = figureId;
+		}
+		
+		editor.insertText('[figure:' + replace + ']');
+	}
+	
+	function addNewFigure(data) {
+		console.log(data);
+		$('<div></div>').dialog();
+	}
+	
+	CKEDITOR.dialog.add('figure', function(editor) {
+		return figureDialog(editor);
+	});
 })(jQuery);
