9a27ca61f80525f995a705e593d4f21283039f34
Ed Bachta <ebachta@imamuseum.org>
2012-02-27 09:40:11 -0500

    Added a preview UI feature for the authoring interface

--- authoring_tools/modules/osci_tk_figure/templates/osci_tk_figure_formatter.tpl.php
+++ authoring_tools/modules/osci_tk_figure/templates/osci_tk_figure_formatter.tpl.php
@@ -1,4 +1,4 @@
-<figure id="<?php print $item['fig_id']; ?>" data-position="<?php print $item['position']; ?>" data-columns="<?php print $item['columns']; ?>" data-options="<?php print htmlspecialchars($item['options']); ?>">
+<figure id="<?php print $item['fig_id']; ?>" title="<?=$item['number_template']?>" data-position="<?php print $item['position']; ?>" data-columns="<?php print $item['columns']; ?>" data-options="<?php print htmlspecialchars($item['options']); ?>">
 	<div class='figure_content'><?= $figure_content ?></div>
 	<figcaption><?php print $item['number_template'] . ' ' . $item['caption']; ?></figcaption>
 </figure>
