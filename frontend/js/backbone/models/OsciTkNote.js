8eb262fe5f0971bf572da0a566303cfcb5e773fb
Gray Bowman <gbowman@imamuseum.org>
2012-03-01 09:56:56 -0500

    added first backbone model

---
+++ frontend/js/backbone/models/OsciTkNote.js
@@ -0,0 +1,23 @@
+var OsciTkNote = Backbone.Model.extend({
+	defaults: function() {
+		return {
+			body: null,
+			section_id: '',
+			paragraph_id: '',
+			tags: []
+		};
+	},
+	
+	sync: function(method, model, options) {
+		var endpoint = '/api/notes';
+		
+		console.log('Note.sync: ' + method);
+		console.log(model, 'model');
+		console.log(options, 'options');
+		
+		if (method == 'create') {
+			var data = {};
+			return $.post(endpoint, data, options);
+		}
+	}
+});
