37efb5f8ced4932b1021df21e487376614c8f777
Gray Bowman <gbowman@imamuseum.org>
2012-03-01 09:58:43 -0500

    added backbone figure model

---
+++ frontend/js/backbone/models/OsciTkFigure.js
@@ -0,0 +1,18 @@
+var OsciTkFigure = Backbone.Model.extend({
+	defaults: function() {
+		return {
+			asset_id: '',
+			section_id: '',
+			delta: '',
+			number_template: '',
+			caption: '',
+			position: '',
+			columns: '',
+			options: {}
+		};
+	},
+	
+	sync: function(method, model, options) {
+		console.log('Note.sync: ' + method);
+	}
+});
