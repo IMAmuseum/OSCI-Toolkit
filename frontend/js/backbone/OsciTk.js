eccc8f1de9d49521403468a2029a1e3bc0c24cbe
Ed Bachta <ebachta@imamuseum.org>
2012-03-01 13:32:40 -0500

    Added helper js file and sample package document

--- frontend/js/backbone/OsciTk.js
+++ frontend/js/backbone/OsciTk.js
@@ -7,37 +7,36 @@ if (!OsciTk) {
 		}
 	};	
 
-	var OsciTk.notes = null;
-	var OsciTk.sections = null;
+	OsciTk.notes = null;
+	OsciTk.sections = null;
 	
 	/* 
-	 * document_url can either be set to the URL of an ePub document (zipped container) 
+	 * document_url can either be set to the URL of an ePub document (zip container) 
 	 * or it can be set to the URL of an ePub package document
 	 */
-	var OsciTk.document_url = null;
+	OsciTk.document_url = null;
 	
 	_.extend(OsciTk, Backbone.Events);
 	
 	OsciTk.init = function() {
+		
 		// get user notes for current user and section
 		OsciTk.notes = new OsciTkNotes;
 		OsciTk.notes.url = OsciTk.settings.endpoints.OsciTkNotes;
 		OsciTk.notes.fetch();		
-
-		OsciTkNotes.fetch();
 		
 		// Initilize section collection
 		// OsciTk.sections = new OsciTkSectionCollection;
 		// Retrieve sections from local storage
 		// OsciTk.sections.fetch();
 		
-		if (OsciTk.sections != null) {
+		if (OsciTk.sections == null) {
 			
+			if (OsciTk.document_url == null) return false;			
 			// TODO: check if the URL points to an ePub or package document. 
 			
 			// Assuming it's a package URL
-			var package_doc = OsciTk.loadPackageDocument(OsciTk.document_url);
-			
+			OsciTk.processPackageDocument(OsciTk.document_url);			
 			
 		}
 	
@@ -48,12 +47,10 @@ if (!OsciTk) {
 	 * Loads the package document so that models can be created for components of the publication
 	 * TODO: determine how this would work if an ePub document is unzipped into memory
 	 */
-	OsciTk.loadPackageDocument(package_url) {
+	OsciTk.processPackageDocument = function(package_url) {
+
+		var data = xmlToJson(loadXMLDoc(package_url));
 		
-		
-		
-		
-	}
-	
+	}	
 	
 }
