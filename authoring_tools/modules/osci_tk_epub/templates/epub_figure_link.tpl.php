73cd12227e1d73118cdc23fece4dce66155a083b
Kyle Jaebker <kjaebker@gmail.com>
2012-03-01 11:10:53 -0500

    Lots of ePub updates
    Added index to navigation document. Fixed ePub validation errors.

--- authoring_tools/modules/osci_tk_epub/templates/epub_figure_link.tpl.php
+++ authoring_tools/modules/osci_tk_epub/templates/epub_figure_link.tpl.php
@@ -1,8 +1,1 @@
-<object id="figref-<?php print $section->nid.'-'.$fig_index.'-'.md5(microtime()); ?>" 
-	class="figref-<?php print $section->nid.'-'.$fig_index; ?>" 
-	type="application/osci-tk-conservation-figure" 
-	data="<?php print $base_url.'node/'.$section->nid.'/'.$fig_index.'/figure.xhtml'; ?>">
-	<div class="figure-reference">
-		<img src="http://www.cgexplorer.com/_sys/images/female-figure-reference.jpg"/>
-	</div>
-</object>
+<a href="#<?php print $fig_id; ?>"><?php print $fig_text; ?></a>
