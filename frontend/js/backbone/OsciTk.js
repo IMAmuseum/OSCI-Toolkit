5ebd15965329adbbdd10447a2b6f4995709fe876
Gray Bowman <gbowman@imamuseum.org>
2012-03-01 10:57:23 -0500

    fetch test

--- frontend/js/backbone/OsciTk.js
+++ frontend/js/backbone/OsciTk.js
@@ -1,12 +1,17 @@
 if (!OsciTk) {
 	var OsciTk = {};
+	OsciTk.settings = {
+		'endpoints': {
+			'OsciTkNotes': '/api/notes/'
+		}
+	};
 	OsciTk.notes = null;
-
 	_.extend(OsciTk, Backbone.Events);
 	
 	OsciTk.init = function() {
 		// get user notes for current user and section
 		OsciTk.notes = new OsciTkNotes;
+		OsciTk.notes.url = OsciTk.settings.endpoints.OsciTkNotes;
 		OsciTk.notes.fetch();
 	}
 }
