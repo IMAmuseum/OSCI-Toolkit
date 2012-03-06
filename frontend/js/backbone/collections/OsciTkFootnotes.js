1829294675a6f871febe7132c42ccf2f9dd2f20e
Gray Bowman <gbowman@imamuseum.org>
2012-03-06 16:16:22 -0500

    debugging

--- frontend/js/backbone/collections/OsciTkFootnotes.js
+++ frontend/js/backbone/collections/OsciTkFootnotes.js
@@ -1,3 +1,6 @@
 var OsciTkFootnotes = Backbone.Collection.extend({
-	model: OsciTkFootnote
+	model: OsciTkFootnote,
+	initialize: function(data) {
+		console.log(data, 'footnotes initialization data');
+	}
 });
