4d72c01f27df78d68979ddb3a32c4e9ddd2c0508
Kyle Jaebker <kjaebker@gmail.com>
2012-03-09 11:51:41 -0500

    adding utf-8 encoding meta tags

--- authoring_tools/modules/osci_tk_epub/templates/osci_tk_epub_container_xml.tpl.php
+++ authoring_tools/modules/osci_tk_epub/templates/osci_tk_epub_container_xml.tpl.php
@@ -1,4 +1,4 @@
-<?php print '<?xml version="1.0"?>'; ?>
+<?php print '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">
     <rootfiles>
         <rootfile full-path="<?php print $rootPath; ?>"
