987eba33ddbf7f58cc2b2440791b5607ec9605cd
Kyle Jaebker <kjaebker@gmail.com>
2012-03-28 10:50:14 -0400

    adding semi-colon after field label

--- authoring_tools/modules/osci_tk_epub/templates/osci_tk_epub_field.tpl.php
+++ authoring_tools/modules/osci_tk_epub/templates/osci_tk_epub_field.tpl.php
@@ -1,6 +1,6 @@
 <section <?php print count($classes_array) ? 'class="' . implode(' ', $classes_array) . '"' : ''; ?>>
 	<?php if(!$label_hidden): ?>
-		<span class="label"><?php print $label; ?></span>
+		<span class="label"><?php print $label; ?>:</span>
 	<?php ENDIF; ?>
 	<?php print render($items); ?>
 </section>
