1bfd1d6fa917410f96a73a5b7172517c57d6bd2d
Gray Bowman <gbowman@imamuseum.org>
2012-04-12 16:20:58 -0400

    user login/logout/register/profile via toolbar

--- frontend/js/backbone/views/OsciTkAccountView.js
+++ frontend/js/backbone/views/OsciTkAccountView.js
@@ -6,14 +6,23 @@ jQuery(function() {
 			this.model = window.appAccount;
 		},
 		render: function() {
-			this.showLoginForm();
+			// determine if user is logged in.  Show login form or user details
+			if (this.model.get('id') > 0) {
+				console.log('user logged in already');
+				this.showProfile();
+			}
+			else {
+				this.showLoginForm();
+			}
 		},
 		events: {
 			'click button.login': 'login',
+			'click button.register': 'register',
 			'click a.register': 'showRegistrationForm',
-			'click a.login': 'showLoginForm'
+			'click a.login': 'showLoginForm',
+			'click a.logout': 'logout',
 		},
-		login: function(event) {
+		login: function() {
 			// alias this for use in ajax callbacks
 			var accountView = this;
 			// get user/pass from form
@@ -31,9 +40,54 @@ jQuery(function() {
 						// user was logged in, set the returned user data
 						accountView.model.set(data.user);
 						console.log(accountView, 'accountView');
+						accountView.showProfile();
 					}
 					else {
 						// user was not logged in, show error
+						accountView.$el.find('div.form-error').html(data.error);
+					}
+				}
+			});
+		},
+		logout: function() {
+			// alias this for use in ajax callback
+			var accountView = this;
+			$.ajax({
+				url: window.appConfig.get('endpoints').OsciTkAccount,
+				data: {action: 'logout'},
+				type: 'POST',
+				dataType: 'json',
+				success: function(data) {
+					console.log(data, 'logout');
+					accountView.model.set(data.user);
+					accountView.showLoginForm();
+				}
+			});
+		},
+		register: function() {
+			// alias for callbacks
+			var accountView = this;
+			// get user/pass from form
+			var username = this.$el.find('#username').val();
+			var password = this.$el.find('#password').val();
+			var email = this.$el.find('#email').val();
+			// send registration request
+			$.ajax({
+				url: window.appConfig.get('endpoints').OsciTkAccount,
+				data: {action: 'register', username: username, password: password, email: email},
+				type: 'POST',
+				dataType: 'json',
+				success: function(data) {
+					console.log(data, 'data');
+					if (data.success === true) {
+						// user was logged in, set the returned user data
+						accountView.model.set(data.user);
+						console.log(accountView, 'accountView');
+						accountView.showProfile();
+					}
+					else {
+						// user was not logged in, show error
+						accountView.$el.find('div.form-error').html(data.error);
 					}
 				}
 			});
@@ -45,6 +99,10 @@ jQuery(function() {
 		showLoginForm: function() {
 			this.template = _.template($('#template-account-login').html());
 			this.$el.html(this.template());
+		},
+		showProfile: function() {
+			this.template = _.template($('#template-account-profile').html());
+			this.$el.html(this.template(this.model.toJSON()));
 		}
 	});
 });
