ebf79cbf9aef64c9840d80ace0efd8bcc27b4bd8
Kyle Jaebker <kjaebker@gmail.com>
2012-04-16 14:50:01 -0400

    multi column updates

--- frontend/js/backbone/views/OsciTkMultiColumnSectionView.js
+++ frontend/js/backbone/views/OsciTkMultiColumnSectionView.js
@@ -1,11 +1,24 @@
 jQuery(function() {
 	window.OsciTkMultiColumnSectionView = OsciTkSectionView.extend({
+		defaults: {
+			minColumnWidth : 200,
+			maxColumnWidth : 300,
+			gutterWidth : 40,
+			minLinesPerColumn : 5,
+			cacheId : null,
+			layoutCacheTime : 86400
+		},
+
 		initialize: function()
 		{
+			console.log(this.options, 'multi options');
+
 			this.dispatcher.on("windowResized", function() {
 				console.log("resized");
 				this.renderContent();
 			}, this);
+
+			this.$el.addClass("oscitk_multi_column");
 
 			OsciTkMultiColumnSectionView.__super__.initialize.call(this);
 		},
@@ -24,20 +37,46 @@ jQuery(function() {
 			var dimensions = {};
 
 			//get the margins of the section container
-			dimensions.sectionMargin = {
+			dimensions.pageMargin = {
 				left : parseInt(this.$el.css("margin-left"), 10),
 				top : parseInt(this.$el.css("margin-top"), 10),
 				right : parseInt(this.$el.css("margin-right"), 10),
 				bottom : parseInt(this.$el.css("margin-bottom"), 10)
 			};
 
+			//get the padding of the section container
+			dimensions.pagePadding = {
+				left : parseInt(this.$el.css("padding-left"), 10),
+				top : parseInt(this.$el.css("padding-top"), 10),
+				right : parseInt(this.$el.css("padding-right"), 10),
+				bottom : parseInt(this.$el.css("padding-bottom"), 10)
+			};
+
 			//determine the correct height for the section container to eliminate scrolling
-			dimensions.sectionHeight = $(window).height() - dimensions.sectionMargin.top - dimensions.sectionMargin.bottom;
+			dimensions.outerPageHeight = $(window).height() - dimensions.pageMargin.top - dimensions.pageMargin.bottom;
+			dimensions.innerPageHeight = dimensions.outerPageHeight - dimensions.pagePadding.top - dimensions.pagePadding.bottom;
+
+			//determine the correct width for the section container
+			dimensions.outerPageWidth = this.$el.width();
+			dimensions.innerPageWidth = dimensions.outerPageWidth - dimensions.pagePadding.left - dimensions.pagePadding.right;
+
+			//column count
+
+			//column width
+			// if (dimensions.innerPageWidth < this.get('maxColumnWidth')) {
+			// 	dimensions.columnWidth = dimensions.innerPageWidth;
+			// } else {
+			// 	dimensions.columnWidth = this.get('maxColumnWidth');
+			// }
+
+			//page count
+
+
+
+			console.log(dimensions, 'dimensions');
 
 			//set the height of the container
-			this.$el.height(dimensions.sectionHeight);
-
-
+			this.$el.height(dimensions.pageHeight);
 		}
 	});
 });
