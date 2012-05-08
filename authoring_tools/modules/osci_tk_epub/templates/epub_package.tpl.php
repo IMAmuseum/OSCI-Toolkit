7d86cfae3ebac3ce579eb745ea5d42a93f2dc543
Gray Bowman <gbowman@imamuseum.org>
2012-05-08 15:42:36 -0400

    Field Nodetree now handles publication hierarchy.  Reworked all of epub for this.  Fixed handful of errors too.

--- authoring_tools/modules/osci_tk_epub/templates/epub_package.tpl.php
+++ authoring_tools/modules/osci_tk_epub/templates/epub_package.tpl.php
@@ -2,20 +2,20 @@
 <package version="3.0" unique-identifier="publication-id" xmlns="http://www.idpf.org/2007/opf" xml:lang="en">
   <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
 	<?php foreach ($metadata as $key => $value): ?>
-		<?= $value ?>
+		<?php print $value ?>
     <?php endforeach ?>
-    <dc:language><?= $language ?></dc:language>
+    <dc:language><?php print $language ?></dc:language>
   </metadata>
   <manifest>
     <?php foreach ($manifest as $item): ?>
       <?php $properties = isset($item['properties']) ? "properties='{$item['properties']}'" : ''; ?>
-	  <item id="<?= $item['id'] ?>" href="<?= $item['href'] ?>" 
-	        media-type="<?= $item['media-type'] ?>" <?= $properties ?> ></item>
+	  <item id="<?php print $item['id'] ?>" href="<?php print $item['href'] ?>" 
+	        media-type="<?php print $item['media-type'] ?>" <?php print $properties ?> ></item>
     <?php endforeach ?>
   </manifest>
   <spine toc="ncxtoc">
     <?php foreach ($spine as $itemref): ?>
-      <itemref idref='<?= $itemref['idref'] ?>'></itemref>
+      <itemref idref='<?php print $itemref['idref'] ?>'></itemref>
     <?php endforeach ?>
   </spine>
 </package>
