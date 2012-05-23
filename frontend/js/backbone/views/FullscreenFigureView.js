9429a00df2119f1d6b9f0ff23ac66e7c485e4d4a
Ed Bachta <ebachta@imamuseum.org>
2012-05-23 16:36:07 -0400

    Added basic fullscreen image figure support

--- frontend/js/backbone/views/FullscreenFigureView.js
+++ frontend/js/backbone/views/FullscreenFigureView.js
@@ -8,9 +8,32 @@ OsciTk.views.FullscreenFigureView = OsciTk.views.BaseView.extend({
 	initialize: function() {
 		app.dispatcher.on('showFigureFullscreen', this.showFigureFullscreen);
 	},
-	showFigureFullscreen: function(event) {
-		console.log('showFigureFullscreen');
-	}
+	showFigureFullscreen: function(id) {
 
+		var figure_model = app.collections.figures.get(id);
+		if (figure_model == undefined) {
+			alert('Error: Figure ' + id + ' not found');
+			return;
+		}
+
+		var figure = null;
+		switch (figure_model.get('type')) {
+			case 'image_asset':
+				var figure = new OsciTk.views.FullscreenImageFigureView({
+					id: id,
+					model: figure_model
+				});
+				break;
+
+			default:
+				console.log('Unsupported figure type', figure_model);
+				return;
+		}
+
+		$.fancybox.open({
+			content: figure.$el.html()
+		});
+
+	}
 
 });
