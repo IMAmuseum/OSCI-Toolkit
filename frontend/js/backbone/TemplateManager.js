2987d7a2f59692850f6346531f2754e473ddc21b
Gray Bowman <gbowman@imamuseum.org>
2012-06-20 09:30:11 -0400

    TemplateManager now supports multiple configurable template paths, which can be used for template overrides

--- frontend/js/backbone/TemplateManager.js
+++ frontend/js/backbone/TemplateManager.js
@@ -4,20 +4,29 @@ if (typeof OsciTk.templates === 'undefined'){OsciTk.templates = {};}
 // OsciTk Namespace Initialization //
 
 OsciTk.templateManager = {
-
 	get : function(templateName) {
-		if (OsciTk.templates[templateName] === undefined) {
-			$.ajax({
-				async : false,
-				dataType : 'html',
-				url : 'js/backbone/templates/' + templateName + '.tpl.html',
-				success : function(data, textStatus, jqXHR) {
-					OsciTk.templates[templateName] = _.template(data);
-				}
-			});
+		return function() {
+			OsciTk.templateManager.useTemplate(templateName);
 		}
-
+	},
+	
+	useTemplate: function(templateName) {
+		if (OsciTk.templates[templateName] === undefined) {
+			var templateUrls = app.config.get('templateUrls');
+			var found = false;
+			for(var i=0; i < templateUrls.length; i++) {
+				$.ajax({
+					async : false,
+					dataType : 'html',
+					url : templateUrl[i] + templateName + '.tpl.html',
+					success : function(data, textStatus, jqXHR) {
+						OsciTk.templates[templateName] = _.template(data);
+						found = true;
+					}
+				});
+				if (found) break;
+			}
+		}
 		return OsciTk.templates[templateName];
 	}
-
 };
