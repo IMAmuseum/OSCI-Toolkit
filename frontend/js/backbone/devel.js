5f049ab982ce881a61116cb3fa80782638f837f0
Kyle Jaebker <kjaebker@gmail.com>
2012-06-27 16:27:53 -0400

    Added listener output to devel script

--- frontend/js/backbone/devel.js
+++ frontend/js/backbone/devel.js
@@ -2,5 +2,26 @@
 var originalBackboneTrigger = Backbone.Events.trigger;
 Backbone.Events.trigger = function() {
 	console.log(arguments, "Event Triggered");
+	
+	var calls = this._callbacks;
+	var eventSplitter = /\s+/;
+	var events = arguments[0].split(eventSplitter);
+	var node, tail;
+	while (event = events.shift()) {
+		if (node = calls[event]) {
+			tail = node.tail;
+			while ((node = node.next) !== tail) {
+				var cid = (node.context !== undefined) ? node.context.cid : undefined;
+				if (cid === undefined && node.context !== undefined && node.context.models) {
+					cid = "a collection";
+				}
+				if (cid === undefined) {
+					cid = "undefined";
+				}
+				console.log([event, node.context], cid + " listened");
+			}
+		}
+	}
+
 	originalBackboneTrigger.apply(this, arguments);
 };
