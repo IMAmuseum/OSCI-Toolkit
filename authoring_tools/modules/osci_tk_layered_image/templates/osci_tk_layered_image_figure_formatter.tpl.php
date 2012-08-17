a55d2a00edd8fa99746d3699c6c72bffb68fd184
Gray Bowman <gray.bowman@gmail.com>
2012-08-17 15:53:12 -0400

    Layered Image module

---
+++ authoring_tools/modules/osci_tk_layered_image/templates/osci_tk_layered_image_figure_formatter.tpl.php
@@ -0,0 +1,5 @@
+<object id="<?php print $item['fig_id']; ?>" 
+	type="<?php print $media_type ?>" 
+	data-url="<?php print $item['content_url']; ?>">
+	<div class="fallback-content"><p>Fallback content</p></div>
+</object>
