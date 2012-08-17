5273b9e4d69c0425608796258c4e31d7bcd3e326
Kris Arnold <karnold@imamuseum.org>
2012-08-17 16:39:14 -0400

    polishing new asset options forms

--- authoring_tools/modules/osci_tk_figure/osci_tk_figure.api.php
+++ authoring_tools/modules/osci_tk_figure/osci_tk_figure.api.php
@@ -5,12 +5,19 @@
  *
  * Options for figure assets must be defined in a custom form
  * per node type.
+ *
+ * You do not need to provide a submit function when creating 
+ * your form function.  All values will be stored in a 
+ * key/value pair.  However any special handling like files
+ * will need to provide a custom submit function to handle 
+ * these cases
  */
-function osci_tk_figure_asset_NODE_TYPE_options_form() {
+function osci_tk_figure_asset_NODE_TYPE_options_form($form, $form_state) {
     $form['options'] = array(
         '#type'     => 'options',
+        '#title'    => t('Some Options'),
         '#value'    => array(1,2,3,4),
     );
 
-    return $form;
+    return osci_tk_figure_asset_options_form($form, $form_state);
 }
