532fa2e05bd7145e291e3ee0d123f19baa72cd4f
Kyle Jaebker <kjaebker@gmail.com>
2012-08-20 11:21:31 -0400

    Allow end notes from a word doc to be processed like footnotes

--- authoring_tools/modules/osci_tk_filters/js/osci_tk_filters.js
+++ authoring_tools/modules/osci_tk_filters/js/osci_tk_filters.js
@@ -9,11 +9,11 @@
 	{
 		return;
 	}
-	
+
 	if (!CKEDITOR.cleanWord) {
 		CKEDITOR.scriptLoader.load(CKEDITOR.basePath + 'plugins/pastefromword/filter/default.js');
 	}
-	
+
 	$('document').ready(function(){
 		$(this).ajaxComplete(function(e, xhr, settings) {
 			if (settings && settings.extraData && settings.extraData._triggering_element_name && settings.extraData._triggering_element_name == 'field_footnote_add_more')
@@ -30,10 +30,10 @@
 						var updateId = $(footnoteContainers[updateContainerNum]).find("textarea").attr("id");
 						CKEDITOR.instances[updateId].setData(window.osci_tk_new_footnotes[i].content);
 					}
-					
+
 					window.osci_tk_new_footnotes = null;
 				}
-				
+
 				$("[name='field_footnote[und][num_add]']").val(1);
 			}
 		});
@@ -46,7 +46,7 @@
 			//  replace footnotes with data
 			var data = $('<span>' + event.data.html + '</span>'),
 				footnotes = [];
-			
+
 			var nextDelta = parseInt($(".field-name-field-footnote").find(".ui-tabs-nav").find("a:last").text(), 10) + 1;
 			nextDelta = nextDelta ? nextDelta : 0;
 
@@ -56,26 +56,26 @@
 					$val = $(val),
 					name = $this.attr('name');
 
-				if (name.indexOf('_ftnref') === 0)
+				if (name.indexOf('_ftnref') === 0 || name.indexOf('_ednref') === 0)
 				{
 					var footnote = name.replace('ref', ''),
 						content  = data.find('#' + footnote.replace('_', ''));
-						
+
 					content.find("a").remove();
 					content = content.text();
 					if (content !== null)
 					{
 						content = CKEDITOR.cleanWord(content, event.editor);
 					}
-					
+
 					$val.replaceWith('[footnoteref:fn-' + Drupal.settings.osci_tk_filters.current_nid + '-' + nextDelta + ']');
-					
+
 					data.find('#' + footnote.replace('_', '')).remove();
 					footnotes.push({
 						footnoteId : footnote,
 						content : content
 					});
-					
+
 					nextDelta++;
 				}
 				else
@@ -90,12 +90,12 @@
 			{
 				$("[name='field_footnote[und][num_add]']").val(numFootnotes);
 				window.osci_tk_new_footnotes = footnotes;
-				
+
 				var addAnother = $(".field-name-field-footnote").find("input.field-add-more-submit");
 				addAnother.trigger("mousedown");
-				
+
 			}
-			
+
 			event.data.html = data.html();
 		});
 	});
