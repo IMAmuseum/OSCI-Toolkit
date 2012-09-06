078dcb7bb17e6991020055bf85102679e49f4835
Apache <apache@ima-stanley.imart.local>
2012-09-06 15:59:58 -0400

    updated figure references

--- authoring_tools/modules/osci_tk_figure/osci_tk_figure.api.php
+++ authoring_tools/modules/osci_tk_figure/osci_tk_figure.api.php
@@ -11,12 +11,17 @@
  * key/value pair.  However any special handling like files
  * will need to provide a custom submit function to handle 
  * these cases
+ *
+ * Values will be stored in $form_state['options'].
  */
 function osci_tk_figure_asset_NODE_TYPE_options_form($form, $form_state) {
-    $form['options'] = array(
+    $form = array();
+
+    $form['my_value'] = array(
         '#type'     => 'options',
         '#title'    => t('Some Options'),
         '#value'    => array(1,2,3,4),
+        '#default_value' => $form_state['options']->my_value,
     );
 
     return osci_tk_figure_asset_options_form($form, $form_state);
