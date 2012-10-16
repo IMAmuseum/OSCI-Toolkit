f47d14d8f82838533a2a18486a6cd1c1ca0d266d
Ed Bachta <ebachta@imamuseum.org>
2012-10-16 14:32:26 -0400

    Added publication metadata to print view
    Issue OSCI-283

---
+++ modules/osci_tk_epub/templates/osci_tk_epub_print.tpl.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+		<?php foreach($css_urls as $url): ?>
+			<link rel='stylesheet' href='<?php echo $url; ?>' />
+		<?php endforeach; ?>
+	</head>
+	<body>
+		<div class='publication-metadata'>
+			<h1><?php print $publication->title; ?></h1>
+			<?php if (isset($publication->field_osci_tk_cover_image['und'])): ?>
+				<img src='<?php print file_create_url($publication->field_osci_tk_cover_image['und'][0]['uri']); ?>' />
+			<?php endif; ?>
+			<?php if (isset($publication->field_osci_tk_creator['und'])): ?>
+				<p class='creator'>
+					<?php 
+						$creators = array();
+						foreach($publication->field_osci_tk_creator['und'] as $creator) {
+							$creators[] = $creator['value'];
+						}
+						echo join(', ', $creators)
+					?>
+				</p>
+			<?php endif; ?>
+			<?php if (isset($publication->field_osci_tk_publisher['und'])): ?>
+				<p class='publisher'>
+					Published by: <?php echo $publication->field_osci_tk_publisher['und'][0]['value']; ?>
+				</p>
+			<?php endif; ?>
+			<?php if (isset($publication->field_osci_tk_rights['und'])): ?>
+				<p class='rights'>
+					<?php echo $publication->field_osci_tk_rights['und'][0]['value']; ?>
+				</p>
+			<?php endif; ?>
+
+			<?php if (isset($publication->field_osci_tk_description['und'])): ?>
+				<p class='description'>
+					<?php echo $publication->field_osci_tk_description['und'][0]['value']; ?>
+				</p>
+			<?php endif; ?>
+		</div>
+		<div class='section-separator'>&nbsp;</div>
+		<?php echo $content; ?>
+	</body>'
+</html>
