fc70ed9ddbee5303f31f30f8252d65bc9c288a87
Daniel Cervantes <dlcerva@gmail.com>
2013-01-08 14:04:23 -0500

    Added glossary module. Added hook to alter package document. Added api doc.

---
+++ modules/osci_tk_epub/osci_tk_epub.api.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Inovked in osci_tk_epub.module to allow additional content to be added
+ * to the package document.
+ * @param  array 	$arguments Renderable array for package document
+ * @param  array 	$data      Data for package document
+ * @param  object 	$node      The publication entity
+ */
+function hook_osci_tk_package_document_alter(&$arguments, $data, $node) {
+
+}
---
+++ modules/osci_tk_glossary/templates/osci_tk_glossary_document.tpl.php
@@ -0,0 +1,6 @@
+<dl>
+<?php foreach ($terms as $term => $definition): ?>
+	<td><?php print $term; ?></td>
+	<dd><?php print $definition; ?></dd>
+<?php endforeach; ?>
+</dl>
