762da1c21df9f01b8a3d2fc375a4749b8c3e1ef0
Daniel Cervantes <dlcerva@gmail.com>
2013-01-15 08:39:23 -0500

    Update modules/osci_tk_epub/osci_tk_epub.api.php
    Fixed typo.

--- modules/osci_tk_epub/osci_tk_epub.api.php
+++ modules/osci_tk_epub/osci_tk_epub.api.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Inovked in osci_tk_epub.module to allow additional content to be added
+ * Invoked in osci_tk_epub.module to allow additional content to be added
  * to the package document.
  * @param  array 	$arguments Renderable array for package document
  * @param  array 	$data      Data for package document
