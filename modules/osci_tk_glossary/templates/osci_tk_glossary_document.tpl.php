023a2833ffa7d900b0817b35d51ab1ecee721e7e
Gray Bowman <gray.bowman@gmail.com>
2013-05-22 11:55:29 -0400

    making glossary standards compliant

--- modules/osci_tk_glossary/templates/osci_tk_glossary_document.tpl.php
+++ modules/osci_tk_glossary/templates/osci_tk_glossary_document.tpl.php
@@ -1,6 +1,12 @@
-<dl>
-<?php foreach ($terms as $name => $term): ?>
-	<td data-tid="<?php print $term['term_id']; ?>"><dfn><?php print $name; ?></dfn></td>
-	<dd><?php print $term['definition']; ?></dd>
-<?php endforeach; ?>
-</dl>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head></head>
+    <body>
+        <dl>
+        <?php foreach ($terms as $name => $term): ?>
+        	<dt data-tid="<?php print $term['term_id']; ?>"><dfn><?php print $name; ?></dfn></dt>
+        	<dd><?php print $term['definition']; ?></dd>
+        <?php endforeach; ?>
+        </dl>
+    </body>
+</html>
