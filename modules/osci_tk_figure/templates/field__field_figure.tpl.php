ba70016390293fdfa93a697ab4cbb5233267834d
Kyle Jaebker <kjaebker@gmail.com>
2013-05-23 23:12:15 -0400

    Correct figure numbering

--- modules/osci_tk_figure/templates/field__field_figure.tpl.php
+++ modules/osci_tk_figure/templates/field__field_figure.tpl.php
@@ -1,7 +1,19 @@
 <section id="figures">
 	<?php 
+		global $osci_tk_placed_figures;
+		$numItems = count($osci_tk_placed_figures);
 		foreach($items as $item)
 		{
+			if ($numItems > 0) {
+				if (array_key_exists($item['#item']['delta'], $osci_tk_placed_figures)) {
+					$item['#item']['number_template'] = $osci_tk_placed_figures[$item['#item']['delta']]['fig_text'];
+				} elseif ($item['#item']['position'] === 'plate') {
+					$item['#item']['number_template'] = "";
+				} else {
+					continue;
+				}
+			}
+				
 			print drupal_render($item); 
 		}
 	?>
