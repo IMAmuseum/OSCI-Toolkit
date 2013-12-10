0cac9af2415bbcaad370e16637722b14a42f1c95
Kyle Jaebker <kjaebker@gmail.com>
2013-12-09 22:58:13 -0500

    override plate image caption

--- modules/osci_tk_figure/templates/osci_tk_figure_formatter.tpl.php
+++ modules/osci_tk_figure/templates/osci_tk_figure_formatter.tpl.php
@@ -1,5 +1,5 @@
 <figure id="<?php print $item['fig_id']; ?>" title="<?php print $item['number_template']?>" data-position="<?php print $item['position']; ?>" data-columns="<?php print $item['columns']; ?>" data-figure_type="<?php print $item['type']; ?>" data-aspect="<?php print $item['aspect']; ?>" data-options="<?php print htmlspecialchars($item['options']); ?>" data-order="<?php print $item['delta']; ?>" data-count="<?php print $item['count']; ?>">
-	<?php 
+	<?php
 		if (isset($item['thumbnail'])) {
 			print $item['thumbnail'];
 		}
@@ -8,5 +8,5 @@
 		}
 	?>
 	<div class='figure_content'><?php print $figure_content ?></div>
-	<figcaption><div><span class="figure_number"><?php print $item['number_template'] . '</span> ' . check_markup($item['value'], $item['format']); ?></div></figcaption>
+	<figcaption><div><?php if (strlen($item['number_template'])) { ?><span class="figure_number"><?php print $item['number_template']; ?></span> <?php } print check_markup($item['value'], $item['format']); ?></div></figcaption>
 </figure>
