f6dd9d9ceeec40d6c3c352e6872d18903dc94885
surreal8 <tinashah8@gmail.com>
2014-05-06 10:53:40 -0500

    sort array by footnote_index

--- modules/osci_tk_epub/templates/osci_tk_footnote_epub.tpl.php
+++ modules/osci_tk_epub/templates/osci_tk_footnote_epub.tpl.php
@@ -1,14 +1,4 @@
 <section epub:type='footnotes' id="footnotes">
-	<?php
-    //sort array by footnote_index
-    function cmp($a, $b)
-    {
-        $myindex1 = (int)$a['footnote_index'];
-        $myindex2 = (int)$b['footnote_index'];
-        return $myindex1 > $myindex2;
-    }
-    usort($items, "cmp");
-    ?>
 	<?php for($i = 0; $i < count($items); $i++):?>
 		<aside epub:type='footnote' id="<?php print $items[$i]['fn_id']; ?>" class="footnote" data-footnote_index="<?php print $items[$i]['footnote_index']; ?>">
 		  <?php print $items[$i]['value']; ?>
