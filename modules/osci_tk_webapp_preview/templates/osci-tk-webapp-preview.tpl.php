4a3b8522ed56d8fd1bd443c6ad34be4c331ce005
surreal8 <tinashah8@gmail.com>
2014-08-27 14:20:18 -0500

    customizing default template

--- modules/osci_tk_webapp_preview/templates/osci-tk-webapp-preview.tpl.php
+++ modules/osci_tk_webapp_preview/templates/osci-tk-webapp-preview.tpl.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="shortcut icon" href="/sites/default/files/favicon.ico" type="image/vnd.microsoft.icon" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
-    <title>OSCI TK Web</title>
+    <title>Online Scholarly Catalogues at the Art Institute of Chicago</title>
 
     <?php foreach($css as $cssPath): ?>
     <link href="<?php print $cssPath; ?>" rel="stylesheet" type="text/css" media="all">
@@ -51,6 +53,16 @@
     </script>
 </head>
 <body>
-    <!-- Content is injected here by backbone -->
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WV5SX7"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WV5SX7');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Content is injected here by backbone -->
 </body>
 </html>
