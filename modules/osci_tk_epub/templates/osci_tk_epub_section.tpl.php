14e7fdbae2a4dee9b23aba7ab1d26af89d8fd817
Jeremy Tubbs <jeremytubbs@gmail.com>
2015-05-07 12:18:40 -0400

    remove dpm

--- modules/osci_tk_epub/templates/osci_tk_epub_section.tpl.php
+++ modules/osci_tk_epub/templates/osci_tk_epub_section.tpl.php
@@ -1,5 +1,4 @@
 <section id="<?php print $field_name; ?>" class="<?php print $classes.' '.$field_name; ?>">
 	<a id="<?php print $field_name; ?>_anchor" class="<?php print $field_name; ?> anchor-link"></a>
-	<?php dpm($content) ?>
 	<?php print $content; ?>
 </section>
