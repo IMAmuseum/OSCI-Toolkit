514fff15424b3755115af51449f51e1f5f4521e1
Jeremy Tubbs <jeremytubbs@gmail.com>
2015-06-09 14:20:35 -0400

    remove duplicate images from epub

--- modules/osci_tk_figure/templates/osci_tk_figure_formatter.tpl.php
+++ modules/osci_tk_figure/templates/osci_tk_figure_formatter.tpl.php
@@ -1,12 +1,4 @@
 <figure id="<?php print $item['fig_id']; ?>" title="<?php print $item['number_template']?>" data-position="<?php print $item['position']; ?>" data-columns="<?php print $item['columns']; ?>" data-figure_type="<?php print $item['type']; ?>" data-aspect="<?php print $item['aspect']; ?>" data-options="<?php print htmlspecialchars($item['options']); ?>" data-order="<?php print $item['delta']; ?>" data-count="<?php print $item['count']; ?>">
-	<?php
-		if (isset($item['thumbnail'])) {
-			print '<img class="thumbnail" src="' . $item['thumbnail'] . '" />';
-		}
-		elseif (isset($thumbnail)) {
-			print $thumbnail;
-		}
-	?>
 	<div class='figure_content'><?php print $figure_content ?></div>
 	<figcaption><div><?php if (strlen($item['number_template'])) { ?><span class="figure_number"><?php print $item['number_template']; ?></span> <?php } print check_markup($item['value'], $item['format']); ?></div></figcaption>
 </figure>
--- modules/osci_tk_layered_image/templates/osci_tk_layered_image_figure_formatter.tpl.php
+++ modules/osci_tk_layered_image/templates/osci_tk_layered_image_figure_formatter.tpl.php
@@ -1,9 +1,7 @@
-<object id="<?php print $item['fig_id']; ?>-object"
+<object id="<?php print $item['fig_id']; ?>"
 	type="<?php print $media_type ?>"
 	data="<?php print $item['content_url']; ?>">
-	<div class="fallback-content">
-		<?php if ($item['thumbnail']): ?>
-		<img src="<?php print($item['thumbnail']); ?>" />
-		<?php endif; ?>
-	</div>
+	<img src="<?php print($item['thumbnail']); ?>" />
 </object>
+
+
