a6f49488cb4a80cdc7e1024b76f372af84e59a5e
root <root@houseparty.(none)>
2016-04-07 18:00:48 -0400

    change theme settings to node tab

--- modules/osci_tk_themes/templates/theme-select.tpl.php
+++ modules/osci_tk_themes/templates/theme-select.tpl.php
@@ -1,25 +1,12 @@
 <?php
 	drupal_add_css(drupal_get_path('module', 'osci_tk_themes') . '/css/osci_tk_themes.css');
-	drupal_add_js(drupal_get_path('module', 'osci_tk_themes') . '/js/osci_tk_themes.js');
+	//drupal_add_js(drupal_get_path('module', 'osci_tk_themes') . '/js/osci_tk_themes.js');
 ?>
 
 <div id="system-themes-page">
-	<h1>OSCI Themes</h1>
 	<div class="system-themes-list system-themes-list-enabled clearfix">
-		<div class="form-item form-type-select">
-			<label>Select a Publication</label>
-			<select id="OsciPubSelect" name="OsciPubId" class="form-select">
-				<option selected disabled>Select a Publication</option>
-				<?php foreach ($data['publications'] as $pub) : ?>
-				<option value="<?php echo $pub['vid']; ?>"><?php echo $pub['title']; ?></option>
-				<?php endforeach; ?>
-			</select>
-		</div>
-		<div id="help">
-			<p>Help Text</p>
-		</div>
 		<?php foreach ($data['publications'] as $pub) : ?>
-		<div id="<?php echo $pub['vid'] . '-theme-list'; ?>" class="theme-list is-hidden">
+		<div id="<?php echo $pub['vid'] . '-theme-list'; ?>" class="theme-list">
 			<h2>Current Theme</h2>
 				<div class="theme-selector clearfix">
 					<img class="screenshot" src="/osci-themes/dist/themes/<?php echo $pub['themes']['current']['name'].'/'.$pub['themes']['current']['screenshot']; ?>">
@@ -33,7 +20,7 @@
 						</ul>
 					</div>
 				</div>
-			<br><br>
+			<hr><br>
 			<h2>Available Themes</h2>
 			<?php foreach ($pub['themes']['others'] as $theme) : ?>
 				<div class="theme-selector clearfix">
