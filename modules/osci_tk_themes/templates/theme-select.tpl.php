3e9bfcca38ffe8e449d29d84780dc4604cac1511
Jeremy Tubbs <jeremytubbs@gmail.com>
2016-04-29 15:49:49 -0400

    node level theme select

--- modules/osci_tk_themes/templates/theme-select.tpl.php
+++ modules/osci_tk_themes/templates/theme-select.tpl.php
@@ -1,28 +1,26 @@
 <?php
 	drupal_add_css(drupal_get_path('module', 'osci_tk_themes') . '/css/osci_tk_themes.css');
-	//drupal_add_js(drupal_get_path('module', 'osci_tk_themes') . '/js/osci_tk_themes.js');
 ?>
 
 <div id="system-themes-page">
 	<div class="system-themes-list system-themes-list-enabled clearfix">
-		<?php foreach ($data['publications'] as $pub) : ?>
-		<div id="<?php echo $pub['vid'] . '-theme-list'; ?>" class="theme-list">
+		<div id="<?php echo $data['pub']['vid'] . '-theme-list'; ?>" class="theme-list">
 			<h2>Current Theme</h2>
 				<div class="theme-selector clearfix">
-					<img class="screenshot" src="/osci-themes/dist/themes/<?php echo $pub['themes']['current']['name'].'/'.$pub['themes']['current']['screenshot']; ?>">
+					<img class="screenshot" src="/osci-themes/dist/themes/<?php echo $data['pub']['themes']['current']['name'].'/'.$data['pub']['themes']['current']['screenshot']; ?>">
 					<div class="theme-info">
-						<h3><?php echo $pub['themes']['current']['name']; ?></h3>
+						<h3><?php echo $data['pub']['themes']['current']['name']; ?></h3>
 						<div class="theme-description"></div>
 						<ul class="operations clearfix">
 							<li class="0 first last">
-								<a href="/admin/config/system/osci/<?php echo $pub['vid']; ?>/theme-settings">Settings</a>
+								<a href="/admin/config/system/osci/<?php echo $data['pub']['vid']; ?>/theme-settings">Settings</a>
 							</li>
 						</ul>
 					</div>
 				</div>
 			<hr><br>
 			<h2>Available Themes</h2>
-			<?php foreach ($pub['themes']['others'] as $theme) : ?>
+			<?php foreach ($data['pub']['themes']['others'] as $theme) : ?>
 				<div class="theme-selector clearfix">
 					<img class="screenshot" src="/osci-themes/dist/themes/<?php echo $theme['name'].'/'.$theme['screenshot']; ?>">
 					<div class="theme-info">
@@ -32,7 +30,7 @@
 							<li class="0 first last">
 								<form method="POST" action="<?php echo $_SERVER['REQUEST_URI'];?>">
 									<input type="hidden" value="<?php echo $theme['name']; ?>" name="theme" />
-									<input type="hidden" value="<?php echo $pub['vid']; ?>" name="vid" />
+									<input type="hidden" value="<?php echo $data['pub']['vid']; ?>" name="vid" />
 									<a href='#' onclick='this.parentNode.submit(); return false;'>
 										Set current
 									</a>
@@ -43,6 +41,5 @@
 				</div>
 			<?php endforeach; ?>
 		</div>
-		<?php endforeach; ?>
 	</div>
 </div>
